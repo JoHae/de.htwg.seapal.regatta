6edfdc7bf7354d6d49f12dfd6140cd073d3a85b2
unknown <roroettg@3DLABOR15.in.fhkn.de>
2013-04-10 11:50:48 +0200

    sd

---
+++ test/test/de/htwg/seapal/regatta/models/mock/ApplicationTest.java
@@ -0,0 +1,20 @@
+package test.de.htwg.seapal.regatta.models.mock;
+
+import org.junit.*;
+
+
+/**
+*
+* Simple (JUnit) tests that can call all parts of a play app.
+* If you are interested in mocking a whole application, see the wiki for more details.
+*
+*/
+public class ApplicationTest {
+
+    @Test
+    public void renderTemplate() {
+        Assert.assertTrue(true);
+    }
+  
+   
+}
