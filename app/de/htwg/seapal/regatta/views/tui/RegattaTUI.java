be238e79f7e3fb8b15704b1be9d670f6affcd4d6
Jojo <johannes.haeussler.87@gmail.com>
2013-04-16 21:51:48 +0200

    refactor

--- app/de/htwg/seapal/regatta/views/tui/RegattaTUI.java
+++ app/de/htwg/seapal/regatta/views/tui/RegattaTUI.java
@@ -22,7 +22,8 @@ public class RegattaTUI implements IObserver, Plugin {
 
 	@Override
 	public boolean processInputLine(String line) {
-
+		String unknownCommand = "Unknown Command! Please try again ...";		
+		
 		boolean continu = true;
 		Scanner scanner = new Scanner(line);
 
@@ -35,14 +36,14 @@ public class RegattaTUI implements IObserver, Plugin {
 			commandLine = line.split(" ");
 			
 			if (commandLine[0].length() != 1) {
-				System.out.println("Unknown Command! Please try again ...");
+				System.out.println(unknownCommand);
 				printTUI();
 				scanner.close();
 				return continu;
 			}
 			
 			command = commandLine[0].charAt(0);
-			
+		
 			switch (command) {
 
 			case 'q':
@@ -51,68 +52,23 @@ public class RegattaTUI implements IObserver, Plugin {
 				break;
 
 			case 'a':
-				if (!scanner.hasNext()) {
-					System.out.println("Missing regatta ID ...");
-					printTUI();
-					break;
-				} else {
-					controller.addRegatta(scanner.next());
-				}
+				addRegattaCommand(scanner);
 				break;
 
 			case 'n':
-				String regattaId;
-				String value;
-				
-				if(!scanner.hasNext()) {
-					System.out.println("Missing regatta ID ...");
-					printTUI();
-					break;
-				} else {
-					regattaId = scanner.next();
-				}
-				
-				if(!scanner.hasNext()) {
-					System.out.println("Missing name value ...");
-					printTUI();
-					break;
-				} else {
-					value = scanner.next();
-				}
-				controller.setRegattaName(regattaId, value);
+				changeNameCommand(scanner);
 				break;
 
 			case 'h':
-				if(!scanner.hasNext()) {
-					System.out.println("Missing regatta ID ...");
-					printTUI();
-					break;
-				} else {
-					regattaId = scanner.next();
-				}
-				
-				if(!scanner.hasNext()) {
-					System.out.println("Missing name value ...");
-					printTUI();
-					break;
-				} else {
-					value = scanner.next();
-				}
-				controller.setRegattaHost(regattaId, value);
+				changeHostCommand(scanner);
 				break;
 
 			case 'p':
-				if (!scanner.hasNext()) {
-					System.out.println("Missing regatta ID ...");
-					printTUI();
-					break;
-				} else {
-					System.out.println(controller.getRegattaString(scanner.next()));
-				}
+				printCommand(scanner);
 				break;
 
 			default:
-				System.out.println("Unknown Command! Please try again ...");
+				System.out.println(unknownCommand);
 			}
 		} catch (Exception ex) {
 			ex.getMessage();
@@ -121,6 +77,68 @@ public class RegattaTUI implements IObserver, Plugin {
 			scanner = null;
 		}
 		return continu;
+	}
+	
+	private void printCommand(Scanner scanner) {
+		if (!scanner.hasNext()) {
+			System.out.println("Missing ID! Please try again ...");
+			printTUI();
+		} else {
+			System.out.println(controller.getRegattaString(scanner.next()));
+		}
+	}
+
+	private void addRegattaCommand(Scanner scanner) {
+		if (!scanner.hasNext()) {
+			System.out.println("Missing ID! Please try again ...");
+			printTUI();
+		} else {
+			controller.addRegatta(scanner.next());
+		}
+	}
+	
+	private void changeNameCommand(Scanner scanner) {
+		String regattaId;
+		String value;
+		
+		if(!scanner.hasNext()) {
+			System.out.println("Missing ID! Please try again ...");
+			printTUI();
+			return;
+		} else {
+			regattaId = scanner.next();
+		}
+		
+		if(!scanner.hasNext()) {
+			System.out.println("Missing name value! Please try again ...");
+			printTUI();
+			return;
+		} else {
+			value = scanner.next();
+		}
+		controller.setRegattaName(regattaId, value);
+	}
+	
+	private void changeHostCommand(Scanner scanner) {
+		String regattaId;
+		String value;
+		
+		if(!scanner.hasNext()) {
+			System.out.println("Missing ID! Please try again ...");
+			printTUI();
+			return;
+		} else {
+			regattaId = scanner.next();
+		}
+		
+		if(!scanner.hasNext()) {
+			System.out.println("Missing hostname value! Please try again ...");
+			printTUI();
+			return;
+		} else {
+			value = scanner.next();
+		}
+		controller.setRegattaHost(regattaId, value);
 	}
 
 	@Override
